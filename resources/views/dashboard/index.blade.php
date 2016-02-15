1a3d6685a82f9221329d1749bac32b3a1d1a4c12
Roger Rojas <roger.rojas.effio@gmail.com>
2016-02-15 18:30:42 -0500

    Add Chat module

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,6 +4,14 @@
 
 @section('title_page', 'Escritorio')
 
+@section('css_inline')
+<style>
+    .chat-body-custom {
+        margin-left: 0px !important;
+    }
+</style>
+@endsection
+
 @section('content')
 
 <div class="row">
@@ -28,8 +36,12 @@
                 </div>
             </a>
 		</div><!-- end of .panel -->
+    </div><!-- end of .col-lg-12 -->
+</div><!-- end of .row -->
 
-		<div class="panel panel-default">
+<div class="row">
+    <div class="col-lg-8">
+		<div class="panel panel-primary">
             <div class="panel-heading">
                 <i class="fa fa-film fa-fw"></i> &Uacute;ltimas peliculas                
             </div>
@@ -74,13 +86,64 @@
                             Ver todas las peliculas <i class="fa fa-arrow-circle-right"></i>
                         </a>
                     </div>
-                    <!-- /.col-lg-12 (nested) -->
+                    <!-- /.col-lg-12 -->
                 </div>
                 <!-- /.row -->
             </div>
             <!-- /.panel-body -->
-        </div>
-	</div><!-- end of .col-lg-12 -->
+        </div><!-- end of .panel -->
+	</div><!-- end of .col-lg-8 -->
+    <div class="col-lg-4">
+        <div id="chat" class="chat-panel panel panel-primary">
+            <div class="panel-heading">
+                <i class="fa fa-comments fa-fw"></i>
+                Chat
+            </div>
+            <div id="chatcontent" class="panel-body">
+                <ul class="chat">
+                </ul><!-- end of .chat -->
+            </div><!-- end of #chatcontent -->
+            <div class="panel-footer">
+                <div class="input-group">
+                    <input id="message" type="text" class="form-control input-sm" placeholder="Escribe tu mensaje aqu&iacute;...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-warning btn-sm" id="btn-chat" type="button">
+                            Enviar!
+                        </button>
+                    </span>
+                </div>
+            </div><!-- end of .panel-footer -->
+        </div><!-- end of #chat -->
+    </div>
+    <!-- /.col-lg-4 -->
 </div><!-- end of .row -->
 
+@endsection
+
+@section('js_plugins')
+<script src="http://localhost:3700/socket.io/socket.io.js"></script>
+@endsection
+@section('js_inline')
+<script>
+    jQuery(function($) {
+
+       var socket = io.connect('http://localhost:3700');
+       var $messageForm = $('#btn-chat');
+       var $message = $('#message');
+       var $chat = $('#chatcontent ul');
+
+       $messageForm.click(function(e) {
+           e.preventDefault();
+           if( $message.val() != "" ) {
+               socket.emit('sendMessage', $message.val());
+               $message.val('');
+           }
+       });
+       
+       socket.on('newMessage', function(data) {
+            var msj = '<li class="left clearfix"><div class="chat-body chat-body-custom"><div class="header"><strong class="primary-font">Roger Rojas</strong><small class="pull-right text-muted"><i class="fa fa-clock-o fa-fw"></i> hace 12 min.</small></div><p class="chat-message">'+ data.msg +'</p></div></li>';
+            $chat.append(msj); 
+       });
+    });
+</script>
 @endsection
