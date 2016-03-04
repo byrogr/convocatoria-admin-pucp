9d501a1c36f6f5e73e880f1d6175655228deaf30
Roger Rojas <roger.rojas.effio@gmail.com>
2016-03-04 12:51:04 -0500

    Fix Envio de email para soporte tecnico

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -17,14 +17,12 @@ class MailController extends Controller
     {
         $data = $request->all();
         $usuario = User::find($request->id);
-        //$data['name'] = $usuario->name;
-        //$data['email'] = $usuario->email;
 
         Mail::send(
             'emails.support',
             $data,
-            function ($message) {
-                $message->from('helpdesk@festivaldelima.com', 'Help Desk');
+            function ($message) use ($usuario) {
+                $message->from($usuario->email, $usuario->name);
                 $message->to($this->to)->cc($this->cc);
                 $message->subject('Helpdesk desde Convocatoria 2016 Festival de Cine de Lima');
             }
